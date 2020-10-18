8dfdd9c46fb84b767bf290265fc5f01be21bde49
grzegorz.bu <grzegorz.bu@modulesgarden.com>
2020-10-18 21:34:03 +0200

    template create

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,12 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea class="w-full" name="body" placeholder="What's up?"></textarea>
+    </form>
+
+    <hr class="my-4">
+
+    <footer class="flex justify-between">
+        <img src="https://picsum.photos/40" alt="" class="rounded-full mr-2">
+        <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+    </footer>
+</div>
