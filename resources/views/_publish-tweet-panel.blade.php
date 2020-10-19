48bfb7688140ac2188bfafaabb69e099f3c3ff13
grzegorz.bu <grzegorz.bu@modulesgarden.com>
2020-10-19 21:33:26 +0200

    add working form

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,12 +1,17 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form method="POST" action="/tweets">
+        @csrf
         <textarea class="w-full" name="body" placeholder="What's up?"></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img src="{{ auth()->user()->avatar }}" alt="your avatar" class="rounded-full mr-2">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+        </footer>
     </form>
 
-    <hr class="my-4">
-
-    <footer class="flex justify-between">
-        <img src="https://picsum.photos/40" alt="" class="rounded-full mr-2">
-        <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
-    </footer>
+    @error('body')
+        <p class="text-red-500 text-sm">{{ $message }}</p>
+    @enderror
 </div>
